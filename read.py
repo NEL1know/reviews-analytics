c314921113da42e3a03936dcfad1b58fe51f358f
Sky Walker <captainsone24@gmail.com>
2019-12-02 20:50:14 +0800

    add filtering function

--- read.py
+++ read.py
@@ -13,3 +13,17 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言的平均長度為' ,sum_len/len(data))
+
+hundred = []
+for d in data:
+	if len(d) < 100:
+		hundred.append(d)
+print('一共有', len(hundred), '筆留言長度小於100')
+print(hundred[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提及good')
+print(good[0])
